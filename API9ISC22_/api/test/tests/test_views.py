75e0e2737ff4e0c7cd7f1997cf0443f1e64888b8
AdrianMiguel05 <2019452089@teschi.edu.mx>
2023-11-30 18:19:29 -0600

    SUBIR

---
+++ API9ISC22_/api/test/tests/test_views.py
@@ -0,0 +1,9 @@
+# tests/test_views.py
+from django.test import TestCase
+from django.urls import reverse
+
+class MyViewTests(TestCase):
+    def test_index_view(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Hello, world!")
