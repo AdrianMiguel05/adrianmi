7c2b250dab851b2dce2b63b4ebcdfcfd4db63455
AdrianMiguel05 <2019452089@teschi.edu.mx>
2024-01-11 23:59:54 -0600

    su

--- API9ISC22/urls.py
+++ API9ISC22/urls.py
@@ -35,6 +35,5 @@ urlpatterns = [
      path('signup/', views.signup,name='signup'),
      path('logout/',views.signout, name='logout'),
      path('enviar_correo/<str:correo>/<str:usuario>/<str:contra>/', views.enviar_correo, name='enviar_correo'),
-     path('encu/', views.Enc, name='encu'),
-    
+     path('index', Home.as_view(),name='index'),
 ]
